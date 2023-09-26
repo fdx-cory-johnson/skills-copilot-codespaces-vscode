0cc7ef1f431925a0507a92206e4e6c3107e9db3b
Cory Johnson <143667452+fdx-cory-johnson@users.noreply.github.com>
2023-09-26 05:04:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,14 +5,3 @@
 const express = require('express');
 const router = express.Router();
 const Comment = require('../models/comment');
-
-// Handle request
-router.get('/', (req, res) => {
-    Comment.find()
-        .then(comments => {
-            res.json(comments);
-        })
-        .catch(err => {
-            res.json({ message: err });
-        });
-});
